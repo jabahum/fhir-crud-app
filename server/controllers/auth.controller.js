f852aee1456a4c1d5ed472a3328150afdcb8211f
jaba <jabajnan8@gmail.com>
2022-06-08 16:03:39 +0300

    added signup of  user

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -7,12 +7,90 @@ const fhir = require("../utils/config");
 // @desc    Register user
 // @route   POST /api/v1/auth/register
 // @access  Public
-exports.signup = asyncHandler(async (req, res, next) => { });
+exports.signup = asyncHandler(async (req, res, next) => {
+
+    let resource = req.body;
+
+    fhir.search("Person", resource.telecom.value)
+        .then((response) => {
+
+            // check if user alreadu existssss`
+            if (response.total > 1) {
+                return res.status(400).json({
+                    success: false,
+                    message: `User already exists`,
+                    error: "",
+                    data: ""
+                })
+            }
+
+
+            // do the rest here 
+            const password = resource.extension.find((ext) => ext.url == "http://lyecdevelopers.com/fhir/StructureDefinition/lyec-password").valueString
+
+            // generate hashPassword
+            const salt = crypto.randomBytes(16).toString('hex')
+            const passwordHash = crypto.pbkdf2Sync(password, salt, 1000, 64, 'sha512').toString('hex')
+
+
+            var passwordExtension = {
+                "url": "http://lyecdevelopers.com/fhir/StructureDefinition/lyec-password",
+                "extension": [
+                    {
+                        "url": "password",
+                        "valueString": passwordHash
+                    },
+                    {
+                        "url": "salt",
+                        "valueString": salt
+                    }
+                ]
+            }
+
+            resource.extension.push(passwordExtension)
+
+
+            // user creation 
+            fhir.create(resource)
+                .then((newUser) => {
+                    if (newUser) {
+                        return res.status(200).json({
+                            success: true,
+                            message: "Successfully SignUp User",
+                            error: "",
+                            data: newUser
+                        })
+                    }
+
+                }).catch((err) => {
+                    res.status(500).json({
+                        success: false,
+                        message: "Failed to SignUp user",
+                        error: err.message,
+                        data: ""
+
+                    })
+
+                })
+
+        }).catch((err) => {
+            return res.status(500).json({
+                success: false,
+                message: "An error occured while  search for users with the same email",
+                error: err.message,
+                data: ""
+            })
+
+        })
+
+});
 
 // @desc    Login user
 // @route   POST /api/v1/auth/login
 // @access  Public
 exports.login = asyncHandler(async (req, res, next) => {
+
+
 
 });
 
